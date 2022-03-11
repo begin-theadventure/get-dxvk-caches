4ad7e847db504e394f87ad8508aa1b681e5cc6ba
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-03-11 17:48:45 +0100

    Update script.

--- script/get-dxvk-caches.sh
+++ script/get-dxvk-caches.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-mkdir ~/dxvk-caches
-
-find . -name '*.dxvk-cache' -type f -print0 | xargs -0 -I{} cp {} ~/caches/
+mkdir -p ~/dxvk-caches
+find . -name '*.dxvk-cache' -type f -print0 | xargs -0 -I{} cp {} ~/dxvk-caches/
